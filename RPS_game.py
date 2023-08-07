d85cd85670aa173557e293dc5dc30c8f7d9154de
Mason101J <60389799+chrisgwx@users.noreply.github.com>
2023-08-06 21:00:33 -0400

    Added grahpics

--- RPS_game.py
+++ RPS_game.py
@@ -3,69 +3,70 @@ Rock Paper Scissors
 -------------------------------------------------------------
 '''
 
-
 import random
 import os
 import re
 
+item = {'R': '🪨', 'S': '✂️', 'P': '📃'}
+
 
 def check_play_status():
-  valid_responses = ['yes', 'no']
-  while True:
-      try:
-          response = input('Do you wish to play again? (Yes or No): ')
-          if response.lower() not in valid_responses:
-              raise ValueError('Yes or No only')
+    valid_responses = ['yes', 'no']
+    while True:
+        try:
+            response = input('Do you wish to play again? (Yes or No): ')
+            if response.lower() not in valid_responses:
+                raise ValueError('Yes or No only')
 
-          if response.lower() == 'yes':
-              return True
-          else:
-              os.system('cls' if os.name == 'nt' else 'clear')
-              print('Thanks for playing!')
-              exit()
+            if response.lower() == 'yes':
+                return True
+            else:
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print('Thanks for playing!')
+                exit()
 
-      except ValueError as err:
-          print(err)
+        except ValueError as err:
+            print(err)
 
 
 def play_rps():
-   play = True
-   while play:
-       os.system('cls' if os.name == 'nt' else 'clear')
-       print('')
-       print('Rock, Paper, Scissors - Shoot!')
+    play = True
+    while play:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print('')
+        print('Rock, Paper, Scissors - Shoot!')
 
-       user_choice = input('Choose your weapon'
-                           ' [R]ock], [P]aper, or [S]cissors: ')
+        user_choice = input('Choose your weapon'
+                            ' [R]ock], [P]aper, or [S]cissors: ')
 
-       if not re.match("[SsRrPp]", user_choice):
-           print('Please choose a letter:')
-           print('[R]ock, [P]aper, or [S]cissors')
-           continue
+        if not re.match("[SsRrPp]", user_choice):
+            print('Please choose a letter:')
+            print('[R]ock, [P]aper, or [S]cissors')
+            continue
 
-       print(f'You chose: {user_choice}')
+        print(f'You Chose: {item[user_choice.upper()]}')
 
-       choices = ['R', 'P', 'S']
-       opp_choice = random.choice(choices)
+        choices = ['R', 'P', 'S']
+        opp_choice = random.choice(choices)
 
-       print(f'I chose: {opp_choice}')
+        print(f'Computer chose: {item[opp_choice.upper()]}')
 
-       if opp_choice == user_choice.upper():
-           print('Tie!')
-           play = check_play_status()
-       elif opp_choice == 'R' and user_choice.upper() == 'S':
-           print('Rock beats scissors, I win!')
-           play = check_play_status()
-       elif opp_choice == 'S' and user_choice.upper() == 'P':
-           print('Scissors beats paper! I win!')
-           play = check_play_status()
-       elif opp_choice == 'P' and user_choice.upper() == 'R':
-           print('Paper beats rock, I win!')
-           play = check_play_status()
-       else:
-           print('You win!\n')
-           play = check_play_status()
+        if opp_choice == user_choice.upper():
+            print('Tie!')
+            play = check_play_status()
+        elif opp_choice == 'R' and user_choice.upper() == 'S':
+            print('Rock beats scissors, I win!')
+            play = check_play_status()
+        elif opp_choice == 'S' and user_choice.upper() == 'P':
+            print('Scissors beats paper! I win!')
+            play = check_play_status()
+        elif opp_choice == 'P' and user_choice.upper() == 'R':
+            print('Paper beats rock, I win!')
+            play = check_play_status()
+        else:
+            print('You win!\n')
+            play = check_play_status()
 
 
 if __name__ == '__main__':
-   play_rps()
+    play_rps()
